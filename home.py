a4107e084e8e86def637fced0d9d869bc56f07fe
inventorywhrmpm-alt <inventory.whrmpm@gmail.com>
2026-05-03 09:08:15 +0700

    Update home.py

--- home.py
+++ home.py
@@ -71,7 +71,7 @@ with col5:
     st.page_link("pages/prediksibyma.py", label="MA Predict", icon="📈")
     
 with col6:
-    st.primary("### 📈 AI Divergence")
+    st.secondary("### 📈 AI Divergence")
     st.write("analisis harga saham Berdasrkan Berdasrkan deteksi Divergnce.")
     st.page_link("pages/prediksibyma.py", label="AI Divergnce", icon="📈")
 
